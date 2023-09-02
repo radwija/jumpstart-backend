7b42a42ab693f81f8e8507ea76ff5a06550421da
radwija <115165737+radwija@users.noreply.github.com>
2023-09-03 00:01:26 +0800

    first commit

---
+++ src/main/java/com/radwija/jumpstartbackend/JumpstartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.radwija.jumpstartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpstartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpstartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radwija/jumpstartbackend/JumpstartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radwija.jumpstartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpstartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
